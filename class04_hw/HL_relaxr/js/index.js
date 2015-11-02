40af72c0d47d27d7a92c76edd6ed238cd86fd203
Hilda LeStrange <hildalestrange@unboxedtechnology.com>
2015-11-01 20:50:15 -0500

    added toggle function to read more

--- class04_hw/HL_relaxr/js/index.js
+++ class04_hw/HL_relaxr/js/index.js
@@ -1,10 +1,35 @@
+// on clicking .show-more
+//   show .story1-full
+//   hide .show-more
+//   show .show-less
+//
+// on clicking .show-less
+//   hide .story1-full
+//   hide .show-less
+//   show .show-more
+
+
 jQuery(document).ready(function(){
 
-  jQuery(".story-link").click(function(event) {
+// Prevent link from scrolling to the top of the page
+  jQuery(".show-more").click(function(event) {
     event.preventDefault();
   });
 
+// When clicking Read More show full story
+  function toggleStory1() {
+    jQuery('#story1-full').slideToggle();
+    jQuery(".show-more").html("Show Less");
+  }
+
+  jQuery('#story1-full').hide();
+  jQuery('.show-more').click(toggleStory1);
+
+
 });
+
+
+
 
   // function question1(){
   //   $("p").css("color", "#14c2e6");
