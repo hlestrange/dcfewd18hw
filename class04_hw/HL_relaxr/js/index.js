ee17df7fd0a64336d46f031f148da189f962b16f
Hilda LeStrange <hildalestrange@unboxedtechnology.com>
2015-11-03 19:33:17 -0500

    used different jquery read more read less toggle

--- class04_hw/HL_relaxr/js/index.js
+++ class04_hw/HL_relaxr/js/index.js
@@ -12,27 +12,48 @@
 jQuery(document).ready(function(){
 
 // Prevent link from scrolling to the top of the page
-  jQuery(".show-more").click(function(event) {
+  jQuery(".toggle-story-1").click(function(event) {
     event.preventDefault();
   });
 
-// When clicking Read More show full story
-  function toggleStory1() {
-    jQuery('#story1-full').slideToggle();
-    jQuery(".show-more").html("Show Less");
-  }
 
-  jQuery('#story1-full').hide();
-  jQuery('.show-more').click(toggleStory1);
+// Read More & Read Less
+
+$(document).ready(function() {
+    var showChar = 600;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Read More";
+    var lesstext = "Read Less";
 
 
+    $('.more').each(function() {
+        var content = $(this).html();
+
+        if(content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="show-more">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".show-more").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 });
 
 
-
-
-  // function question1(){
-  //   $("p").css("color", "#14c2e6");
-  // }
-  //
-	// $("#button1").click(question1);
+});
