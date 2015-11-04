d881a144d28c6bd147f8f54f39438664a51ce9c1
Hilda LeStrange <hildalestrange@unboxedtechnology.com>
2015-11-03 20:12:39 -0500

    wrote my own jquery for showing hiding learn more section

--- class04_hw/HL_relaxr/js/index.js
+++ class04_hw/HL_relaxr/js/index.js
@@ -12,48 +12,27 @@
 jQuery(document).ready(function(){
 
 // Prevent link from scrolling to the top of the page
-  jQuery(".toggle-story-1").click(function(event) {
+  jQuery("a").click(function(event) {
     event.preventDefault();
   });
 
 
-// Read More & Read Less
-
-$(document).ready(function() {
-    var showChar = 600;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "Read More";
-    var lesstext = "Read Less";
-
-
-    $('.more').each(function() {
-        var content = $(this).html();
-
-        if(content.length > showChar) {
-
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
-
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="show-more">' + moretext + '</a></span>';
-
-            $(this).html(html);
-        }
-
-    });
-
-    $(".show-more").click(function(){
-        if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-        } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
-    });
+// Learn More
+$( "#learn-more-link" ).click(function() {
+  $("#learn-more-link").hide();
+  $( "#learn-more-text" ).slideDown( "slow", function() {
+  });
+  $("#learn-less-link").show();
 });
 
+$( "#learn-less-link" ).click(function() {
+  $("#learn-less-link").hide();
+  $( "#learn-more-text" ).slideUp( "slow", function() {
+  });
+  $("#learn-more-link").show();
+});
+
+$("#learn-more-text").hide();
+$("#learn-less-link").hide();
 
 });
